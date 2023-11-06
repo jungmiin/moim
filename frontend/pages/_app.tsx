382dbd0f17804d06eef7a1211d16a518d4807a6a
jungmiin <jungpush@gmail.com>
2023-11-06 20:25:29 +0900

    refactor : modify header

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,19 +4,32 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import Script from "next/script";
 import ToastProvider from "@/components/common/toast/context";
+import Head from "next/head";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 config.autoAddCss = false;
+const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps) => {
+  // Create a client
+
   return (
     <>
+      <Head>
+        <title>moim : 오늘부턴 약속도 간편하게</title>
+      </Head>
       {globalStyles}
       <Script
         src="https://kit.fontawesome.com/f970c48591.js"
         crossOrigin="anonymous"
       ></Script>
-      <ToastProvider>
-        <Component {...pageProps} />
-      </ToastProvider>
+      <QueryClientProvider client={queryClient}>
+        <ToastProvider>
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </ToastProvider>
+      </QueryClientProvider>
     </>
   );
 };
